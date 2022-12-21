6adc39c204aebf8c41a365ce429631cef172c664
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-12-21 18:15:28 +0300

    added blogPosts endpoints and 404 error handler

--- src/server.js
+++ src/server.js
@@ -1,15 +1,33 @@
 import express from "express";
+import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./api/authors/index.js";
-// import authorsRouter from "./api/authors";
+import blogPostsRouter from "./api/blogPosts/index.js";
+import cors from "cors";
+import { notFoundHandler } from "./errorHandlers.js";
 
 const server = express();
 // server waits for request
 
-server.use(express.json());
-
 const port = 3001;
 
+// ***************** MIDDLEWARES ********************
+// const authorMiddleWare = (req, res, next) => {
+
+// }
+
+// Just to let FE communicate with BE successfully
+server.use(cors());
+
+// If you do not add this line here BEFORE the endpoints, all req.body will be UNDEFINED
+server.use(express.json());
+
+// ****************** ENDPOINTS *********************
 server.use("/authors", authorsRouter);
-server.listen(port, () => {
-  console.log("Server is running on port:", port);
-});
+server.use("/blogPosts", blogPostsRouter);
+
+// ****************** ERROR HANDLERS ****************
+server.use(notFoundHandler), //404
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log("Server is running on port:", port);
+  });
