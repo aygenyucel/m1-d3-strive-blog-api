fb432f6b523ab8eff211acfe3bb96c4897d72a7e
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-12-22 14:59:00 +0300

    added exp middlewares

--- src/server.js
+++ src/server.js
@@ -10,24 +10,48 @@ const server = express();
 
 const port = 3001;
 
-// ***************** MIDDLEWARES ********************
-// const authorMiddleWare = (req, res, next) => {
+// ************************* MIDDLEWARES ****************************
+//order matters
+//global middlewares needs to be displaying before endpoints
+const loggerMiddleWare = (req, res, next) => {
+  console.log("req.header:", req.headers);
+  console.log(
+    `Request method ${req.method} -- url ${req.url} -- ${new Date()}`
+  );
+  console.log("req.user:", req.user);
 
-// }
+  req.user = "Riccardo";
+  console.log("req.user:", req.user);
+  console.log("xxxxxxxxxx", req);
 
+  next(); //gives the control to whom is coming next (either another middleware or route handler)
+};
+
+// const policeOfficerMiddleware = (req, res, next) => {
+//   console.log("Current user:", req.user);
+//   if (req.user === "Riccardo") {
+//     res.status(403).send({ message: "Riccardos are not allowed" });
+//   } else {
+//     next();
+//   }
+// };
+
+server.use(loggerMiddleWare);
+// server.use(policeOfficerMiddleware);
 // Just to let FE communicate with BE successfully
 server.use(cors());
-
 // If you do not add this line here BEFORE the endpoints, all req.body will be UNDEFINED
 server.use(express.json());
 
-// ****************** ENDPOINTS *********************
+// ************************** ENDPOINTS *****************************
 server.use("/authors", authorsRouter);
 server.use("/blogPosts", blogPostsRouter);
 
-// ****************** ERROR HANDLERS ****************
-server.use(notFoundHandler), //404
-  server.listen(port, () => {
-    console.table(listEndpoints(server));
-    console.log("Server is running on port:", port);
-  });
+// ************************** ERROR HANDLERS ************************
+server.use(notFoundHandler); //404
+
+// **************************************************
+server.listen(port, () => {
+  console.table(listEndpoints(server));
+  console.log("Server is running on port:", port);
+});
